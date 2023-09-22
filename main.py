beb8153c5b3012372247563468e1bada360d42e7
GavrikGal <gavrik.gal@gmail.com>
2023-09-22 10:58:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from service import read_data, plot_line, plot_stats, plot_min_max, mean_p_value, setup_grid, stats
+
+
+first_file = r'z:\0. Данные\SA\data\traces\2023\Антенны\П6-23М\Перед поверкой\750-2000 МГц [tr1-ГП, tr2-ВП].csv'
+# sec_file = r'z:\0. Данные\SA\data\traces\2023\Антенны\П6-23М\После поверки\750-2000 МГц ГП_3_центр.csv'
+files = [
+    r'z:\0. Данные\SA\data\traces\2023\Антенны\П6-23М\После поверки\750-2000 МГц ГП_3_лево.csv',
+    r'z:\0. Данные\SA\data\traces\2023\Антенны\П6-23М\После поверки\750-2000 МГц ГП_3_право.csv',
+    r'z:\0. Данные\SA\data\traces\2023\Антенны\П6-23М\После поверки\750-2000 МГц ГП_3_центр.csv',
+]
+
+df1 = read_data(first_file, 1)
+# df2 = read_data(sec_file, 6)
+
+df2 = pd.DataFrame()
+for file in files:
+    df2 = pd.concat([df2, read_data(file, 6)], axis=1)
+df2_stats = stats(df2)
+
+fig, ax = plt.subplots(figsize=(12, 8))
+setup_grid(ax, f_min=600, f_max=2100, f_tick=200, fm_tick=25, y_min=40, y_max=95, y_tick=10, ym_tick=1)
+
+plot_stats(ax, df2_stats, color='tab:blue')
+plot_min_max(ax, df2_stats, color='tab:blue')
+plot_line(ax, df1, color='tab:red')
+
+df2_stats.to_csv('results.csv')
+
+mean_p_value = mean_p_value(df1, df2, df2_stats)
+plt.title(f'Средняя вероятность, принадлежности первой выборки ко второй: {mean_p_value*100:.2f}%')
+plt.legend()
+plt.show()
---
+++ service.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+
+def read_data(path: str, col_n: int) -> pd.DataFrame:
+    df = pd.read_csv(path, sep=',', encoding='utf-8', usecols=range(col_n+1), skiprows=45,
+                     names=['F, MHz'] + [f'E{i}, dBuV/m' for i in range(1, col_n+1)],
+                     index_col=0)
+    df.index /= 1000000
+    return df
+
+
+def stats(df: pd.DataFrame) -> pd.DataFrame:
+    df_out = pd.DataFrame()
+    df_out['mean'] = df.mean(axis=1)
+    df_out['min'] = df.min(axis=1)
+    df_out['max'] = df.max(axis=1)
+    df_out['sem'] = df.sem(axis=1)
+    df_out['interval-'], df_out['interval+'] = st.t.interval(confidence=0.95, df=len(df.columns) - 1,
+                                                             loc=df_out['mean'], scale=df_out['sem'])
+    return df_out
+
+
+def plot_stats(ax, df_stats: pd.DataFrame, color=None) -> None:
+    ax.plot(df_stats['mean'], linewidth=1.0, label='Среднее после поверки')
+    ax.fill_between(df_stats.index.values, df_stats['interval-'], df_stats['interval+'], color=color,
+                    alpha=0.5, linewidth=0.4, label='Доверительный интервал')
+
+
+def plot_min_max(ax, df_stats: pd.DataFrame, color=None) -> None:
+    ax.fill_between(df_stats.index.values, df_stats['min'], df_stats['max'], color=color, alpha=0.15,
+                    linewidth=0.5, label='Мин-Макс значения')
+
+
+def plot_line(ax, df: pd.DataFrame, color=None) -> None:
+    ax.plot(df, linewidth=1.2, color=color, label='До поверки')
+
+
+def setup_grid(ax, f_min, f_max, f_tick, fm_tick, y_min, y_max, y_tick, ym_tick):
+    ax.set(ylim=(y_min, y_max))
+
+    major_xticks = np.arange(f_min, f_max, f_tick)
+    minor_xticks = np.arange(f_min, f_max, fm_tick)
+    major_yticks = np.arange(y_min, y_max, y_tick)
+    minor_yticks = np.arange(y_min, y_max, ym_tick)
+
+    ax.set_xticks(major_xticks)
+    ax.set_xticks(minor_xticks, minor=True)
+    ax.set_yticks(major_yticks)
+    ax.set_yticks(minor_yticks, minor=True)
+
+    ax.grid(visible=True, alpha=0.3)
+    ax.grid(visible=True, which='minor', alpha=0.05)
+
+
+def mean_p_value(df1, df2, df2_stats):
+    t_value, p_value = st.ttest_1samp(df2, df1, axis=1)
+    df2_stats['t-value'] = t_value
+    df2_stats['p-value'] = p_value
+    mean_p_value = df2_stats['p-value'].mean()
+    return mean_p_value
